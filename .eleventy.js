e537352b3b5d8f10806f869b199cd5ca10c6ac7d
Fathur Rohman <fathur.rohman@julofinance.com>
2025-12-15 12:32:57 +0700

    update content

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy({ "src/icons": "icons" });
 
   return {
     dir: {
